9a1391b56ed1bad9867edd85f4f1868e1ad758f3
khiempham2004 <youremail@example.com>
2026-06-23 23:39:42 +0700

    fix vercel api connect

--- server.js
+++ server.js
@@ -1,12 +1,41 @@
-import app, { connectDB } from './app.js';
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import routesUser from './routes/User.routes.js';
+import routesCourse from './routes/Course.routes.js';
+import routesEnroll from './routes/Enrollment.routes.js';
+import routeSchedule from './routes/Schedule.routes.js';
+import path from 'path';
 
+dotenv.config();
+
+const app = express();
 const PORT = process.env.PORT || 9000;
 
-connectDB()
-    .then(() => {
-        app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-    })
-    .catch(err => {
-        console.error(err);
-        process.exit(1);
-    });
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(cors());
+
+// Static files
+app.use("/images", express.static("images"));
+app.use("/uploads", express.static(path.join(process.cwd(), "uploads"))
+);
+
+// Routes
+app.use('/api/users', routesUser);
+app.use('/api/courses', routesCourse);
+app.use('/api/enrollments', routesEnroll);
+app.use('/api/schedules', routeSchedule);
+
+app.get('/api', (req, res) => {
+    res.send('Hello World!');
+});
+
+mongoose.connect(process.env.MONGO_URI, {
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true,
+})
+    .then(app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
+    .catch(err => console.error(err));
+
